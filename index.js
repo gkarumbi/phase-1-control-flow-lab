cda09e01db0e63d7dbbee2ca3693df036e2578ea
George Karumbi <gkarumbi@gmail.com>
2022-08-03 13:27:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue === 199){
+    return "This one is on me!"
+  }
+  else if(someValue > 400 && 2000 > someValue){
+    return "That will be twenty bucks."
+  }
+  else if(someValue>2000){
+    return "I will gladly take your thirty bucks."
+  }
+  else if(someValue > 2500){
+    return "No can do."
+
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   const cityMessage = city ==='NYC'|| ' Pittsburgh'? 'Ok, sounds good.' : 'No go'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+  
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
 }
